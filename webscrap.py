9e5ee1a5ef971ed0ab203fb41d9d6be40b98bb3e
MuzammilPIAIC <50516983+MuzammilPIAIC@users.noreply.github.com>
2022-01-03 20:35:30 +0500

    Update webscrap.py

--- webscrap.py
+++ webscrap.py
@@ -8,6 +8,7 @@ driver.get("https://www.airliftexpress.com/search/daal")
 # resource https://www.geeksforgeeks.org/extract-all-the-urls-from-the-webpage-using-python/
 # https://stackoverflow.com/questions/64474877/unable-to-scrape-products-price-from-amazon
 # https://stackoverflow.com/questions/55268753/scraping-amazon-products-names
+# https://www.youtube.com/watch?v=_AeudsbKYG8
 
 
 #url = 'https://www.airliftexpress.com/search/daal'
